d77733e0d41d69b2badfc83be9b89b1927b04684
Marci Prescott <mjprescott@me.com>
2024-01-13 18:15:00 -0700

    Added challenge4.py

---
+++ challenge4.py
@@ -0,0 +1,7 @@
+# Inputs a list.
+# Outputs a sum of the elements in the list.
+# Easy option using sum function, it takes an iterable as
+# its imnput and returns the sume of the elements.
+
+number_list = [1, 2, 3, 4, 5, 6, 20, 88]
+print(sum(number_list))
