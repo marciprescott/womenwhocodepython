70679e5770051131552d03b50ce408bbfc12d843
Marci Prescott <mjprescott@me.com>
2024-01-14 19:41:22 -0700

    Added challenge5.py

---
+++ challenge5.py
@@ -0,0 +1,17 @@
+list_1 = [9, 0, 8, 6, 2, 18]
+list_1.sort()
+print(list_1)
+
+
+def get_min(list):
+    return list[0]
+
+
+print(get_min(list_1))
+
+
+def get_max(list):
+    return list[-1]
+
+
+print(get_max(list_1))
