4b9102b24f2f446757b36651cd0667420d7b9892
Marci Prescott <mjprescott@me.com>
2024-01-15 23:27:39 -0700

    Added challenge6.py

---
+++ challenge6.py
@@ -0,0 +1,20 @@
+# Write a program to count the occurrences of a specific character in a string
+# Inputs - a string
+# Outputs - number of occurences of specific char
+
+# Get user input & test
+string = input("Enter string to process: ")
+
+char = input("Enter character to tabuate: ")
+
+
+def count_character(string, char):
+    count = 0
+
+    for character in string:
+        if character == char:
+            count += 1
+    return count
+
+
+print(count_character(string, char))
