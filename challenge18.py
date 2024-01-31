92c0a1396457c0d897b576e1d0c8c3137bab3e34
Marci Prescott <mjprescott@me.com>
2024-01-30 23:40:57 -0700

    Finished challenge18.py

--- challenge18.py
+++ challenge18.py
@@ -1,1 +1,13 @@
 # Create a program to find the largest among three numbers.
+# Create a program to find the largest among three numbers.
+
+
+def greatest_of_three(x, y, z):
+    return max(x, y, z)
+
+
+x = int(input("Enter first number: "))
+y = int(input("Enter second number: "))
+z = int(input("Enter third number: "))
+
+print("The biggest number is: ", max(x, y, z))
