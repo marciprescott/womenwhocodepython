5fb6f6a97d9de9178b1c9ca9ee5c953382091502
Marci Prescott <mjprescott@me.com>
2024-01-31 15:06:23 -0700

    Updated challenge 19

--- challenge19.py
+++ challenge19.py
@@ -1,1 +1,13 @@
-# Write a function to calculate the factorial of a number.
+""" A function to calculate the factorial of a number calculated using recursion."""
+
+
+def fact(n):
+    # Base case
+    if n == 0 or n == 1:
+        return 1
+    else:
+        return n * fact(n - 1)
+
+
+number = int(input("Enter number to calculate factorial: "))
+print("The result of calling factorial on the number ", number, "is", fact(number))
