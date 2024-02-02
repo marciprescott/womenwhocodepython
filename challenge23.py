a7f37b34fd9e0894440d53d58b051c2ca90c8dde
Marci Prescott <mjprescott@me.com>
2024-02-01 17:03:51 -0700

    Fixed spelling error in print statement

--- challenge23.py
+++ challenge23.py
@@ -7,7 +7,7 @@ def does_key_exist(key):
     if dic.get(key) == None:
         print("The key you entered IS NOT in the dictionary!")
     else:
-        print("The ke you entered IS in the dictionary!")
+        print("The key you entered IS in the dictionary!")
 
 
 key = input("Enter key to verify existence: ")
