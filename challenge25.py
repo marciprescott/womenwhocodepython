cbe321982148f8c7c30e97fc32157543ead9d31f
Marci Prescott <mjprescott@me.com>
2024-02-03 17:52:31 -0700

    Completed day 25 challenge.

---
+++ challenge25.py
@@ -0,0 +1,13 @@
+""" A program to concatenate two lists. """
+
+
+# concatenate takes two lists as inputs and returns a concatenated list.
+def concatenate(list_1, list2):
+    return list_1 + list2
+
+
+# List examples
+ages = [19, 26, 29]
+languages = ["Python", "Swift", "C++"]
+# Call the function
+print(concatenate(ages, languages))
