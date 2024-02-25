075728fc02489cff585de64af04345f1ee40143b
Marci Prescott <mjprescott@me.com>
2024-02-24 18:36:02 -0700

    Added day 46

---
+++ challenge46.py
@@ -0,0 +1,21 @@
+"""A function to check if a given list is sorted"""
+
+
+def is_sorted(lst):
+    # enumerate(lst[:-1]) allows iterating over
+    # the list lst excluding the last element,
+    # as we're comparing each element with the next one.
+    # Within the loop, index holds the index of cur element,
+    # and value holds the value of the cur element.
+    for index, value in enumerate(lst[:-1]):
+        if value > lst[index + 1]:
+            return False
+    return True
+
+
+# Example usage:
+my_list = [1, 2, 3, 4, 5]
+print(is_sorted(my_list))  # Output: True
+
+my_list = [5, 4, 3, 2, 1]
+print(is_sorted(my_list))  # Output: False
